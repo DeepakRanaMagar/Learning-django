5a6e20a518c2fe09ee565ad19179e3f7a83cf3c9
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-01-25 04:16:16 +0545

    Day 2, Creating a project

--- second_project/second_project/settings.py
+++ second_project/second_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8iu7^fh)2cht6#+rk5#clpo!t86jzin7ud!janfv#9n6+o*p26'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
