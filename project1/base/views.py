170acde3ea52df28d94bdc6c70f6f29c9e4d985b
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-01-27 06:29:23 +0545

    Day3, mySocial

--- project1/base/views.py
+++ project1/base/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 # Create your views here.
 
 rooms = [
-    {'id':1, 'name':'Hello world'},
-    {'id':2, 'name':'Hello c'},
-    {'id':3, 'name':'Hello 1'},
+    {'id':1, 'name':'Reels'},
+    {'id':2, 'name':'Marketplace'},
+    {'id':3, 'name':'Profile'},
 ]
 
 def home(request):
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id']==int(pk):
+            room = i 
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
